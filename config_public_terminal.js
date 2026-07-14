a1393c58ebf18d2ffa7c4dc1cfafe19d20e853f4
Lucasinator5000 <lucasinator5911@gmail.com>
2026-07-13 19:31:56 -0600

    Update config_public_terminal.js

--- config_public_terminal.js
+++ config_public_terminal.js
@@ -1,23 +1,39 @@
-// The root OS image location, change to local filepath if serving locally
-export const diskImageUrl = "wss://disks.webvm.io/debian_buster_large_permis_fixed_01-06-2026.ext2";
-// The root filesystem backend type use "cloud" for serving remotely or "bytes" for serving locally
-export const diskImageType = "cloud";
+// config_public_terminal.js
+
+// Phase 1 Execution: Explicitly lock down 2 GB of RAM (32,768 Wasm pages)
+const totalPages = 32768; 
+export const cxMemory = new WebAssembly.Memory({
+    initial: totalPages,
+    maximum: totalPages,
+    shared: true // Required for multi-threaded SharedArrayBuffer execution
+});
+
+// Update to point to your custom-compiled image location
+export const diskImageUrl = "/custom-disk-images/debian_vizon.ext2";
+
+// Switched to "bytes" to fetch our standalone local/hosted .ext2 file
+export const diskImageType = "bytes";
+
 // Print an introduction message about the technology
-export const printIntro = true;
+export const printIntro = false; // Set to false for a clean custom boot loader experience
+
 // Is a graphical display needed
 export const needsDisplay = false;
+
 // Executable full path (Required)
 export const cmd = "/bin/bash";
+
 // Arguments, as an array (Required)
 export const args = ["--login"];
+
 // Optional extra parameters
 export const opts = {
-	// Environment variables
-	env: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
-	// Current working directory
-	cwd: "/home/user",
-	// User id
-	uid: 1000,
-	// Group id
-	gid: 1000
+    // Environment variables
+    env: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
+    // Current working directory
+    cwd: "/home/user",
+    // User id
+    uid: 1000,
+    // Group id
+    gid: 1000
 };
